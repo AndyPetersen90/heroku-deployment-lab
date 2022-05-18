8abbf613d66a4a99790d48f5140b43e3465f46be
Andrew Petersen <fespetersen1990@gmail.com>
2022-05-18 14:57:36 -0600

    added get app for css file

--- server/index.js
+++ server/index.js
@@ -3,10 +3,14 @@ const path = require('path');
 
 const app = express();
 
-app.use("/", express.static(path.join(__dirname, "../public")));
+// app.use("/", express.static(path.join(__dirname, "../public")));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"))
+})
+
+app.get("/styles", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.css"))
 })
 
 const port = process.env.PORT || 5005
