fb6fd2293b015c5bfcb0b6bd320323fd25d0d023
Andrew Petersen <fespetersen1990@gmail.com>
2022-05-18 14:38:09 -0600

    added an endpoint

--- server/index.js
+++ server/index.js
@@ -5,3 +5,6 @@ const app = express();
 
 app.use("/", express.static(path.join(__dirname, "../public")));
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+})
