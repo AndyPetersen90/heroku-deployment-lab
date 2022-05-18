0c2e572466a5a5d1b2891c0b21b151c72ab359b6
Andrew Petersen <fespetersen1990@gmail.com>
2022-05-18 15:05:01 -0600

    got css to link properly

--- server/index.js
+++ server/index.js
@@ -9,9 +9,12 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"))
 })
 
-app.get("/styles", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.css"))
 })
+
+
+
 
 const port = process.env.PORT || 5005
 
